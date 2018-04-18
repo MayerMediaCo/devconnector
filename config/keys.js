35b439983765ccae102068149c9b1d5adced1f42
danmayer4l <dannymayer@Dannys-MacBook-Pro.local>
2018-04-17 21:30:31 -0500

    added authentication and routing to app

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://danny:danny@ds141474.mlab.com:41474/devconnectortuts"
+  mongoURI: "mongodb://danny:danny@ds141474.mlab.com:41474/devconnectortuts",
+  secretOrKey: "secret"
 };
